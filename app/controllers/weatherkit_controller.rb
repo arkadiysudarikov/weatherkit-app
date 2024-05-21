0d8f28f3aabad8cf0eae784c384a88ceb6e4bb17
Arkadiy (RKD) Sudarikov <arkadiy@arkadiy.com>
2024-05-20 21:09:30 -0700

    Update weatherkit_controller.rb

--- app/controllers/weatherkit_controller.rb
+++ app/controllers/weatherkit_controller.rb
@@ -2,6 +2,8 @@ require "jwt"
 require "openssl"
 
 class WeatherkitController < ApplicationController
+  include AppleApiClient
+
   # CACHE_EXPIRATION = 30.0.minutes
   CACHE_EXPIRATION = 10.0.seconds
 
@@ -55,20 +57,5 @@ class WeatherkitController < ApplicationController
 
   def weatherkit_params
     params.permit(:address)
-  end
-
-  def api_call(url, token)
-    uri = URI.parse(url)
-
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    http.start
-
-    request = Net::HTTP::Get.new(uri.request_uri)
-    request.add_field("Authorization", "Bearer #{token}")
-
-    response = http.request(request)
-
-    JSON.parse(response.body)
   end
 end
