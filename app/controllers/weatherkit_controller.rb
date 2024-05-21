cfe6c4ffd9bbf2cb8bc485196dd06297d0f12a20
Arkadiy (RKD) Sudarikov <arkadiy@arkadiy.com>
2024-05-20 20:15:42 -0700

    Update weatherkit_controller.rb

--- app/controllers/weatherkit_controller.rb
+++ app/controllers/weatherkit_controller.rb
@@ -12,12 +12,6 @@ class WeatherkitController < ApplicationController
       key_id = Rails.application.credentials.apple.key_id
       service_id = Rails.application.credentials.apple.service_id
 
-      Rails.logger.debug "private_key: #{private_key}"
-      Rails.logger.debug "team_id: #{team_id}"
-      Rails.logger.debug "key_id: #{key_id}"
-      Rails.logger.debug "service_id: #{service_id}"
-
-
       jwt = JWT.encode({
         iss: team_id,
         iat: Time.now.to_i,
@@ -31,9 +25,6 @@ class WeatherkitController < ApplicationController
         id: "#{team_id}.#{service_id}"
       })
 
-
-      Rails.logger.debug "jwt: #{jwt.inspect}"
-
       @token = api_call("https://maps-api.apple.com/v1/token", jwt)
 
       token = @token["accessToken"]
@@ -43,13 +34,7 @@ class WeatherkitController < ApplicationController
       longitude = @mapkit["results"][0]["coordinate"]["longitude"]
       latitude = @mapkit["results"][0]["coordinate"]["latitude"]
 
-
-
-
-
       @zipcode = @mapkit["results"][0]["structuredAddress"]["postCode"]
-
-      Rails.logger.info "zipcode: #{@zipcode}"
 
       @weather = if @zipcode.present?
 
@@ -60,22 +45,9 @@ class WeatherkitController < ApplicationController
 
           api_call("https://weatherkit.apple.com/api/v1/weather/en/#{latitude}/#{longitude}?dataSets=currentWeather%2CforecastDaily&timezone=America%2FLos_Angeles", jwt)
         end
-
-
       else
         api_call("https://weatherkit.apple.com/api/v1/weather/en/#{latitude}/#{longitude}?dataSets=currentWeather%2CforecastDaily&timezone=America%2FLos_Angeles", jwt)
       end
-
-
-
-
-
-
-      Rails.logger.info "cache_miss: #{@cache_miss}"
-
-
-
-
     end
   end
 
@@ -89,18 +61,13 @@ class WeatherkitController < ApplicationController
     uri = URI.parse(url)
 
     http = Net::HTTP.new(uri.host, uri.port)
-
     http.use_ssl = true
-
     http.start
 
     request = Net::HTTP::Get.new(uri.request_uri)
-
     request.add_field("Authorization", "Bearer #{token}")
 
     response = http.request(request)
-
-    Rails.logger.info "response: #{response.body}"
 
     JSON.parse(response.body)
   end
