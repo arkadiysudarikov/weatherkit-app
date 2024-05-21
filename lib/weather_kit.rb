f21359f3037869e112d483e91318cf88de0d3de3
Arkadiy (RKD) Sudarikov <arkadiy@arkadiy.com>
2024-05-20 22:14:37 -0700

    Update weather_kit.rb

--- lib/weather_kit.rb
+++ lib/weather_kit.rb
@@ -1,8 +1,9 @@
 module WeatherKit
   include Jwt
-  include AppleApiClient
 
   def weatherkit(latitude, longitude)
-    api_call("https://weatherkit.apple.com/api/v1/weather/en/#{latitude}/#{longitude}?dataSets=currentWeather%2CforecastDaily&timezone=America%2FLos_Angeles", jwt)
+    api_client = AppleApiClient.new
+
+    api_client.api_call("https://weatherkit.apple.com/api/v1/weather/en/#{latitude}/#{longitude}?dataSets=currentWeather%2CforecastDaily&timezone=America%2FLos_Angeles", jwt)
   end
 end
