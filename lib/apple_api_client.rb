83b0bc39a9e949c79e1ea016aea2c2aecd3a18bc
Arkadiy (RKD) Sudarikov <arkadiy@arkadiy.com>
2024-05-20 21:38:38 -0700

    Update apple_api_client.rb

--- lib/apple_api_client.rb
+++ lib/apple_api_client.rb
@@ -1,24 +1,4 @@
 module AppleApiClient
-  def jwt
-    private_key = Rails.application.credentials.apple.private_key
-    team_id = Rails.application.credentials.apple.team_id
-    key_id = Rails.application.credentials.apple.key_id
-    service_id = Rails.application.credentials.apple.service_id
-
-    JWT.encode({
-      iss: team_id,
-      iat: Time.now.to_i,
-      exp: 30.minutes.from_now.to_i,
-      sub: service_id
-    },
-    OpenSSL::PKey::EC.new("-----BEGIN PRIVATE KEY-----\n#{private_key}\n-----END PRIVATE KEY-----"),
-    "ES256",
-    {
-      kid: key_id,
-      id: "#{team_id}.#{service_id}"
-    })
-  end
-
   def api_call(url, token)
     uri = URI.parse(url)
 
