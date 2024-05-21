31f88d3a18f33d3da56a9285fc0c752ed23e2621
Arkadiy (RKD) Sudarikov <arkadiy@arkadiy.com>
2024-05-20 23:02:14 -0700

    Update map_kit.rb

--- lib/map_kit.rb
+++ lib/map_kit.rb
@@ -1,13 +1,11 @@
 module MapKit
   include Jwt
 
-  def mapkit
+  def mapkit(address)
     api_client = AppleApiClient.new
 
-    @token = api_client.api_call("https://maps-api.apple.com/v1/token", jwt)
+    token = api_client.api_call("https://maps-api.apple.com/v1/token", jwt)["accessToken"]
 
-    token = @token["accessToken"]
-
-    api_client.api_call("https://maps-api.apple.com/v1/geocode?q=#{params[:address]}", token)
+    api_client.api_call("https://maps-api.apple.com/v1/geocode?q=#{address}", token)["results"].first
   end
 end
